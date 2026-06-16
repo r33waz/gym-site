76d121af819cf344f53ba6b2eb0ef19f89eede89
r33waz <reewazthapa22@gmail.com>
2026-06-16 23:30:43 +0545

    fix: use Options hook

--- frontend/src/hooks/useOptions.ts
+++ frontend/src/hooks/useOptions.ts
@@ -1,12 +1,23 @@
-function useSelectOptions<T, V extends string | number>(
-  data: T[],
-  labelKey: keyof T,
-  valueKey: keyof T
-): { label: string; value: V }[] {
-  return data.map((item) => ({
-    label: String(item[labelKey]),
-    value: item[valueKey] as V, // keep original type
-  }));
+import { useMemo } from "react";
+
+type Option = {
+  label: string;
+  value: unknown;
+};
+
+function useOptions<T, L extends keyof T, V extends keyof T>(
+  data: T | T[],
+  labelKey: L,
+  valueKey: V,
+) {
+  return useMemo<Option[]>(() => {
+    const items = Array.isArray(data) ? data : [data];
+
+    return items.map((item) => ({
+      label: String(item[labelKey]),
+      value: item[valueKey],
+    }));
+  }, [data, labelKey, valueKey]);
 }
 
-export default useSelectOptions; 
+export default useOptions;
