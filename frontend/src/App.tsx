db9fb21b34d8b00fe104e856fd43f3003228a5cf
r33waz <riwaz.thapa@yenyasoft.com>
2026-07-22 12:39:03 +0545

    chore: fix the husky will all the required structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,6 @@
 import Login from "./module/public/login/login";
-import useOptions from "./hooks/useOptions";
 
 function App() {
-  const users = [
-    { id: 1, name: "John Doe", age: 10 },
-    { id: 2, name: "Jane Smith", age: 20 },
-  ];
-
-  const options = useOptions(users, "age", "id");
-
   return (
     <>
       <Login />
