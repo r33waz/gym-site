243af740a2f9dc9330ba7bf2dc4db4943206eeb5
r33waz <riwaz.thapa@yenyasoft.com>
2026-07-22 11:31:13 +0545

    feat: added the husky in the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
 
 import Login from "./module/public/login/login";
+import useOptions from "./hooks/useOptions";
 
 function App() {
+  const users = [
+    { id: 1, name: "John Doe", age: 10 },
+    { id: 2, name: "Jane Smith", age: 20 },
+  ];
+
+  const options = useOptions(users, "age", "id");
+  console.log("🚀 ~ App ~ options:", options)
+  console.log("🚀 ~ App ~ options:", options)
+  console.log("🚀 ~ App ~ options:", options)
+
   return (
     <>
       <Login/>
