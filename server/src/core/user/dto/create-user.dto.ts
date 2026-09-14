5806f18387858e377282605ae0e79f5cc2b51577
r33waz <reewazthapa22@gmail.com>
2026-09-14 23:54:25 +0545

    feat: statued thenes js

--- server/src/core/user/dto/create-user.dto.ts
+++ server/src/core/user/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNumber()
+  id: number;
+  
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsEnum(['admin', 'user'], { message: 'Role must be either admin or user' })
+  @IsOptional()
+  role: 'admin' | 'user';
+
+  @IsBoolean()
+  @IsOptional()
+  isActive: boolean;
+}
--- server/src/core/user/user.controller.ts
+++ server/src/core/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -12,8 +12,12 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
+  // @Get()
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
+  @Get('/all')
+  getUser() {
     return this.userService.findAll();
   }
 
@@ -24,7 +28,7 @@ export class UserController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return { data: updateUserDto, message: `User ${id} Updated Sucessfully` };
   }
 
   @Delete(':id')
